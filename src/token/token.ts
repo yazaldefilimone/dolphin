e4d637afc1900f01c12d9be7b4e22f7261e8ee8f
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-06-29 21:47:18 +0200

    feat: create tokens

---
+++ src/token/token.ts
@@ -0,0 +1,28 @@
+export class Token {
+  type: TokenType;
+  literal: string;
+  constructor(type: TokenType, literal: string) {
+    this.type = type;
+    this.literal = literal;
+  }
+}
+export enum TokenType {
+  ILLEGAL = "ILLEGAL",
+  EOF = "EOF",
+  // Identifiers + literals
+  IDENT = "IDENT", // add, foobar, x, y, ...
+  INT = "INT", // 1343456
+  // Operators
+  ASSIGN = "=",
+  PLUS = "+",
+  // Delimiters
+  COMMA = ",",
+  SEMICOLON = ";",
+  LPAREN = "(",
+  RPAREN = ")",
+  LBRACE = "{",
+  RBRACE = "}",
+  // Keywords
+  FUNCTION = "FUNCTION",
+  LET = "LET",
+}
