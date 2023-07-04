420344fa1df540ddd923be1e3d262a8f15f01175
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-04 23:04:40 +0200

    feat(repl): create read eval print loop

---
+++ src/repl/repl.ts
@@ -0,0 +1,32 @@
+import readline from "readline";
+import { Lexer } from "lexer";
+import { TokenType } from "token";
+
+const ScannerClose = {
+  exit: "exit",
+  quit: "quit",
+};
+
+const exits = [ScannerClose.exit, ScannerClose.quit];
+
+export function startReadEvalPrintLoop() {
+  const scanner = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  function repl() {
+    scanner.question("> ", (input) => {
+      if (exits.includes(input)) return scanner.close();
+      const lexer = new Lexer(input);
+      let token = lexer.nextToken();
+      while (token.type !== TokenType.EOF) {
+        console.log(token);
+        token = lexer.nextToken();
+      }
+      repl();
+    });
+  }
+  console.log("Welcome!");
+  repl();
+}
