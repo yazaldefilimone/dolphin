6694cda6b3261c4e4a938516741a0d9846abebed
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-17 22:49:14 +0200

    refactor(ast): rename `let-statement` to `let`

--- src/ast/let.ts
+++ src/ast/let.ts
@@ -5,9 +5,10 @@ import { Identifier } from "./identifier";
 export class LetStatement implements Statement {
   token: Token;
   name: Identifier;
-  value: Expression;
+  value: Expression | null;
   constructor(token: Token) {
     this.token = token;
+    this.value = null;
   }
 
   tokenLiteral(): string {
@@ -16,5 +17,14 @@ export class LetStatement implements Statement {
 
   statementNode(): void {
     throw new Error("Method not implemented.");
+  }
+  toString(): string {
+    const out_string = this.tokenLiteral().concat(" ");
+    out_string.concat(this.name.toString()).concat(" = ");
+    if (this.value !== null) {
+      console.log(this.value);
+      out_string.concat(this.value.toString());
+    }
+    return out_string.concat(";");
   }
 }
