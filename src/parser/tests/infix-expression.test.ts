962637442b993c71ba31a7029679b78a8677e562
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-22 17:16:10 +0200

    test(parser): infix operators

---
+++ src/parser/tests/infix-expression.test.ts
@@ -0,0 +1,74 @@
+import { ExpressionStatement, InfixExpression, IntegerLiteral } from "ast";
+import { it, expect, describe } from "vitest";
+import { makeSut } from "./sut";
+
+describe("Parser", () => {
+  describe("parse program", () => {
+    it("infix expression", () => {
+      const infixTests = [
+        {
+          input: "5 + 5;",
+          leftValue: 5,
+          operator: "+",
+          rightValue: 5,
+        },
+        {
+          input: "5 - 5;",
+          leftValue: 5,
+          operator: "-",
+          rightValue: 5,
+        },
+        {
+          input: "5 * 5;",
+          leftValue: 5,
+          operator: "*",
+          rightValue: 5,
+        },
+        {
+          input: "5 / 5;",
+          leftValue: 5,
+          operator: "/",
+          rightValue: 5,
+        },
+        {
+          input: "5 > 5;",
+          leftValue: 5,
+          operator: ">",
+          rightValue: 5,
+        },
+        {
+          input: "5 < 5;",
+          leftValue: 5,
+          operator: "<",
+          rightValue: 5,
+        },
+        {
+          input: "5 == 5;",
+          leftValue: 5,
+          operator: "==",
+          rightValue: 5,
+        },
+        {
+          input: "5 != 5;",
+          leftValue: 5,
+          operator: "!=",
+          rightValue: 5,
+        },
+      ];
+      infixTests.forEach((infixTest) => {
+        const { program } = makeSut(infixTest.input, true, true);
+        expect(program.statements.length === 1).toBeTruthy();
+        const statement: any = program.statements[0];
+        expect(statement).toBeInstanceOf(ExpressionStatement);
+        const expression = statement.expression;
+        expect(expression).toBeInstanceOf(InfixExpression);
+        expect(expression.tokenLiteral()).toEqual(infixTest.operator);
+        expect(expression.operator).toEqual(infixTest.operator);
+        expect(expression.left).toBeInstanceOf(IntegerLiteral);
+        expect(expression.left.value).toEqual(infixTest.leftValue);
+        expect(expression.right).toBeInstanceOf(IntegerLiteral);
+        expect(expression.right.value).toEqual(infixTest.rightValue);
+      });
+    });
+  });
+});
