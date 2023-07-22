874a7d289f68cd6a50f1ceea62a833df63a3e730
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-22 18:12:03 +0200

    tests(parser): operator precedence parsing

---
+++ src/parser/tests/operactor-precedence-parsing.test.ts
@@ -0,0 +1,33 @@
+import { it, expect, describe } from "vitest";
+import { makeSut } from "./sut";
+
+describe("Parser", () => {
+  describe("parse program", () => {
+    it("operator precedence parsing", () => {
+      const tests = [
+        { input: "-a * b", expected: "((-a) * b)" },
+        { input: "!-a", expected: "(!(-a))" },
+        { input: "a + b + c", expected: "((a + b) + c)" },
+        { input: "a + b - c", expected: "((a + b) - c)" },
+        { input: "a * b * c", expected: "((a * b) * c)" },
+        { input: "a * b / c", expected: "((a * b) / c)" },
+        { input: "a + b / c", expected: "(a + (b / c))" },
+        {
+          input: "a + b * c + d / e - f",
+          expected: "(((a + (b * c)) + (d / e)) - f)",
+        },
+        { input: "3 + 4; -5 * 5", expected: "(3 + 4)((-5) * 5)" },
+        { input: "5 > 4 == 3 < 4", expected: "((5 > 4) == (3 < 4))" },
+        { input: "5 < 4 != 3 > 4", expected: "((5 < 4) != (3 > 4))" },
+        {
+          input: "3 + 4 * 5 == 3 * 1 + 4 * 5",
+          expected: "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+        },
+      ];
+      tests.forEach((tt) => {
+        const { program } = makeSut(tt.input, true, true);
+        expect(program.toString()).toBe(tt.expected);
+      });
+    });
+  });
+});
