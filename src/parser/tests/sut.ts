06df5e80b3d1babc97ae024604a16912425ba258
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-22 18:10:49 +0200

    refactor(parser/tests): method for debug in test mod

--- src/parser/tests/sut.ts
+++ src/parser/tests/sut.ts
@@ -1,9 +1,20 @@
+import { Program } from "ast";
 import { Lexer } from "lexer";
 import { ErrorHandler, Parser } from "parser";
-function printError(errors: ErrorHandler) {
-  errors.getErrorsInString().map((message) => {
-    console.log({ error: message });
+function debugPrintError(errors: ErrorHandler) {
+  errors.getErrors().map((error) => {
+    console.log({ error });
   });
+}
+
+function debugPrintErrorInString(errors: ErrorHandler) {
+  errors.getErrorsInString().map((message) => {
+    const error = "Error: " + message.concat("\n");
+    console.log("\x1b[31m%s\x1b[0m", error);
+  });
+}
+function debugToString(program: Program) {
+  console.log(program.toString().concat("\n"));
 }
 export const makeSut = (code: string, isLogError = false, toString = false) => {
   const lexer = new Lexer(code);
@@ -12,7 +23,7 @@ export const makeSut = (code: string, isLogError = false, toString = false) => {
   const errors = parser.errorHandler.getErrors();
   const errorsInString = parser.errorHandler.getErrorsInString();
   const errorHandler = parser.errorHandler;
-  toString && console.log(program.toString());
-  isLogError && printError(parser.errorHandler);
+  toString && debugToString(program);
+  isLogError && debugPrintErrorInString(parser.errorHandler);
   return { lexer, parser, program, errors, errorsInString, errorHandler };
 };
