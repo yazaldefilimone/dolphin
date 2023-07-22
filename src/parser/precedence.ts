30e7a1211e3b353f92d73548bd85e51f50a95386
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-22 17:13:57 +0200

    feat(parser): create hashmap of precedence

--- src/parser/precedence.ts
+++ src/parser/precedence.ts
@@ -1,3 +1,5 @@
+import { TokenType } from "token";
+
 export enum Precedence {
   LOWEST = 1,
   EQUALS, // ==
@@ -7,3 +9,16 @@ export enum Precedence {
   PREFIX, // -X or !X
   CALL, // myFunction(X)
 }
+
+type precedenceMapType = Partial<Record<TokenType, Precedence>>;
+export const precedences: precedenceMapType = {
+  [TokenType.LPAREN]: Precedence.LOWEST,
+  [TokenType.ASTERISK]: Precedence.PRODUCT,
+  [TokenType.SLASH]: Precedence.PRODUCT,
+  [TokenType.LT]: Precedence.LESSGREATER,
+  [TokenType.GT]: Precedence.LESSGREATER,
+  [TokenType.PLUS]: Precedence.SUM,
+  [TokenType.MINUS]: Precedence.SUM,
+  [TokenType.EQ]: Precedence.EQUALS,
+  [TokenType.NOT_EQ]: Precedence.EQUALS,
+};
