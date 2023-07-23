dbcad5ac72b93d5255ee79952ef91e18d17ca6bf
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-23 18:41:42 +0200

    tests(parser): tests boolean literal

---
+++ src/parser/tests/boolean.test.ts
@@ -0,0 +1,33 @@
+import { it, expect, describe } from "vitest";
+import { makeSut } from "./sut";
+import { BooleanLiteral, ExpressionStatement, LetStatement } from "ast";
+describe("Parser", () => {
+  describe("Parse program", () => {
+    it("boolean literal", () => {
+      const code = `
+    true;
+    false;
+    `;
+      const tests = [
+        { input: "true;", expected: "true", value: true },
+        { input: "false;", expected: "false", value: false },
+      ];
+      const { program } = makeSut(code, {
+        isLogError: true,
+        toString: false,
+      });
+      tests.forEach((tt, index) => {
+        const statement: ExpressionStatement = program.statements[index] as any;
+        const expression = statement.expression as BooleanLiteral;
+        console.log({ statement });
+        expect(expression).instanceOf(BooleanLiteral);
+        expect(expression.tokenLiteral()).toBe(tt.expected);
+        expect(expression.value).toBe(tt.value);
+        // TODO: fix this
+        // const expression = statement.value;
+        // expect(expression).toBeInstanceOf(BooleanLiteral);
+        // expect(expression.tokenLiteral()).toEqual(tt.expected);
+      });
+    });
+  });
+});
