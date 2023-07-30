302282b893934b0066c37c057cf8573ed8175d4a
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-30 23:13:16 +0200

    test(parser): parse function with parameters and no body

--- src/parser/tests/function-literal.test.ts
+++ src/parser/tests/function-literal.test.ts
@@ -67,5 +67,25 @@ describe("Parser", () => {
       expect(body?.statements.length).toBe(0);
       expect(statement.toString()).toBe("fn() {}");
     });
+    it("parse function literal with parameters and no body", () => {
+      let code = "fn(x, y) {}";
+      const { program } = makeSut(code, {
+        isLogError: true,
+        toString: false,
+      });
+      expect(program.statements.length).toBe(1);
+      const statement = program.statements[0] as ExpressionStatement;
+      expect(statement.tokenLiteral()).toBe("fn");
+      expect(statement).toBeInstanceOf(ExpressionStatement);
+      expect(statement.expression).toBeInstanceOf(FunctionLiteral);
+      const functionLiteral = statement.expression as FunctionLiteral;
+      expect(functionLiteral.parameters?.length).toBe(2);
+      expect(functionLiteral.parameters?.[0].toString()).toBe("x");
+      expect(functionLiteral.parameters?.[1].toString()).toBe("y");
+      expect(functionLiteral.body).toBeInstanceOf(BlockStatement);
+      const body = functionLiteral.body as BlockStatement;
+      expect(body?.statements.length).toBe(0);
+      expect(statement.toString()).toBe("fn(x,y) {}");
+    });
   });
 });
