1a2c2c7c7ae9372a7274a40d683479c89ab581cf
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-07-30 23:06:01 +0200

    test(parser): parse function literal

---
+++ src/parser/tests/function-literal.test.ts
@@ -0,0 +1,32 @@
+import { it, expect, describe } from "vitest";
+import { makeSut } from "./sut";
+import { FunctionLiteral } from "ast/function";
+import { BlockStatement, ExpressionStatement } from "ast";
+
+describe("Parser", () => {
+  describe("parseProgram", () => {
+    it("parse function literal", () => {
+      let code = "fn(x, y) { x + y; }";
+      const { program } = makeSut(code, {
+        isLogError: true,
+        toString: false,
+      });
+      expect(program.statements.length).toBe(1);
+      const statement = program.statements[0] as ExpressionStatement;
+      expect(statement.tokenLiteral()).toBe("fn");
+      expect(statement).toBeInstanceOf(ExpressionStatement);
+      expect(statement.expression).toBeInstanceOf(FunctionLiteral);
+      const functionLiteral = statement.expression as FunctionLiteral;
+      expect(functionLiteral.parameters?.length).toBe(2);
+      expect(functionLiteral.parameters?.[0].toString()).toBe("x");
+      expect(functionLiteral.parameters?.[1].toString()).toBe("y");
+      expect(functionLiteral.body).toBeInstanceOf(BlockStatement);
+      const body = functionLiteral.body as BlockStatement;
+      expect(body?.statements.length).toBe(1);
+      expect(body?.statements[0]).toBeInstanceOf(ExpressionStatement);
+      const bodyStatement = body?.statements[0] as ExpressionStatement;
+      expect(bodyStatement?.toString()).toBe("(x + y)");
+      expect(statement.toString()).toBe("fn(x,y) {(x + y)}");
+    });
+  });
+});
