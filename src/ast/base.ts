d559ac7f8eb15f83c031ae2a9e72988d47dd2732
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-08-05 23:51:31 +0200

    feat(ast): kind of nodes

--- src/ast/base.ts
+++ src/ast/base.ts
@@ -1,12 +1,38 @@
+export enum ProgramKind {
+  program = "program",
+}
+export enum StatementKind {
+  LET = "LET",
+  RETURN = "RETURN",
+  EXPRESSION = "EXPRESSION",
+  BLOCK = "BLOCK",
+}
+export enum ExpressionKind {
+  IDENTIFIER = "IDENTIFIER",
+  INTEGER = "INTEGER",
+  BOOLEAN = "BOOLEAN",
+  STRING = "STRING",
+  PREFIX = "PREFIX",
+  INFIX = "INFIX",
+  IF = "IF",
+  FUNCTION = "FUNCTION",
+  CALL = "CALL",
+}
+
+type nodeKindType = StatementKind | ExpressionKind | ProgramKind;
+
 export interface Node {
+  kind: nodeKindType;
   tokenLiteral(): string;
   toString(): string;
 }
 
 export interface Statement extends Node {
+  kind: StatementKind | ProgramKind;
   statementNode(): void;
 }
 
 export interface Expression extends Node {
+  kind: ExpressionKind;
   expressionNode(): void;
 }
