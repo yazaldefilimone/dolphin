664b94ba24ee226b19fa0fa8996e6faa797f043a
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-08-05 23:26:11 +0200

    feat(evaluator): create initial eval

---
+++ src/evaluator/evaluator.ts
@@ -0,0 +1,9 @@
+import { Node, Program } from "ast";
+import { BaseObject } from "evaluator/object";
+import { Maybe } from "utils";
+// export class Evaluator {
+//   constructor(program: Program) {}
+// }
+export function Evaluator(program: Maybe<Node>): Maybe<BaseObject> {
+  return null;
+}
