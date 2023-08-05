0dc258f793730bd8112e7abd35f0353eab843f5f
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-08-06 00:19:38 +0200

    feat(evaluator): evaluator integer

--- src/evaluator/evaluator.ts
+++ src/evaluator/evaluator.ts
@@ -1,9 +1,36 @@
-import { Node, Program } from "ast";
+import {
+  Node,
+  ProgramKind,
+  StatementKind,
+  ExpressionKind,
+  Statement,
+  Program,
+  Expression,
+  ExpressionStatement,
+  IntegerLiteral,
+} from "ast";
 import { BaseObject } from "evaluator/object";
 import { Maybe } from "utils";
-// export class Evaluator {
-//   constructor(program: Program) {}
-// }
-export function Evaluator(program: Maybe<Node>): Maybe<BaseObject> {
-  return null;
+import { Integer } from "./object/integer";
+
+export function Evaluator(node: Maybe<Node>): Maybe<BaseObject> {
+  if (node === null) return null;
+  switch (node?.kind) {
+    case ProgramKind.program:
+      return evaluatorStatements((node as Program).statements);
+    case StatementKind.EXPRESSION:
+      return Evaluator((node as ExpressionStatement).expression);
+    case ExpressionKind.INTEGER:
+      return new Integer((node as IntegerLiteral).value);
+    default:
+      return null;
+  }
+}
+
+function evaluatorStatements(statements: Statement[]): Maybe<BaseObject> {
+  let result: Maybe<BaseObject> = null;
+  for (const statement of statements) {
+    result = Evaluator(statement);
+  }
+  return result;
 }
