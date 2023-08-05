fd02d56acd7eea8471251e055800b8d1dae071fe
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-08-05 23:27:04 +0200

    tests(evaluator): integer expression

---
+++ src/evaluator/tests/integer-expression.test.ts
@@ -0,0 +1,22 @@
+import { it, expect, describe } from "vitest";
+import { makeSut } from "./sut";
+
+describe("Evaluator", () => {
+  it("evaluator integer expression", () => {
+    const tests = [
+      {
+        input: "5",
+        expected: 5,
+      },
+      {
+        input: "10",
+        expected: 10,
+      },
+    ];
+
+    tests.forEach((tt) => {
+      const evaluated = makeSut(tt.input);
+      expect(evaluated).toEqual(tt.expected);
+    });
+  });
+});
