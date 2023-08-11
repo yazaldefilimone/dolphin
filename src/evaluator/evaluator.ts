5f23c6addb71acceeafc84134e4d5510c03e95d4
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-08-12 00:05:23 +0200

    feat(evaluator): infix expression

--- src/evaluator/evaluator.ts
+++ src/evaluator/evaluator.ts
@@ -9,6 +9,7 @@ import {
   IntegerLiteral,
   BooleanLiteral,
   PrefixExpression,
+  InfixExpression,
 } from "ast";
 import { BaseObject, InternalBoolean, Integer, internal, objectType } from "evaluator/object";
 
@@ -27,9 +28,14 @@ export function Evaluator(node: Maybe<Node>): Maybe<BaseObject> {
       return nativeBoolToBooleanObject((node as BooleanLiteral).value);
     case ExpressionKind.PREFIX:
       const prefixNode = node as PrefixExpression;
-      const right = Evaluator(prefixNode.right);
-      if (right === null) return null;
-      return evaluatorPrefixExpression(prefixNode.operator, right);
+      const prefixRight = Evaluator(prefixNode.right);
+      if (prefixRight === null) return null;
+      return evaluatorPrefixExpression(prefixNode.operator, prefixRight);
+    case ExpressionKind.INFIX:
+      const infix = node as InfixExpression;
+      const infixLeft = Evaluator(infix.left);
+      const infixRight = Evaluator(infix.right);
+      return evaluatorInfixExpression(infixLeft, infix.operator, infixRight);
     default:
       return null;
   }
@@ -72,6 +78,42 @@ function evaluatorBangOperatorExpression(right: BaseObject): BaseObject {
     default:
       return internal.FALSE;
   }
+}
+
+function evaluatorInfixExpression(left: Maybe<BaseObject>, op: string, right: Maybe<BaseObject>): Maybe<BaseObject> {
+  if (!isObject(left) || !isObject(right)) {
+    return null;
+  }
+  const l = left as BaseObject<number>;
+  const r = right as BaseObject<number>;
+  switch (op) {
+    case "+":
+      return new Integer(l.value + r.value);
+    case "/":
+      return new Integer(l.value / r.value);
+    case "*":
+      return new Integer(l.value * r.value);
+    case "-":
+      return new Integer(l.value - r.value);
+    case "<":
+      return nativeBoolToBooleanObject(l.value < r.value);
+    case ">":
+      return nativeBoolToBooleanObject(l.value > r.value);
+    case "==":
+      return nativeBoolToBooleanObject(l.value === r.value);
+    case "!=":
+      return nativeBoolToBooleanObject(l.value !== r.value);
+    default:
+      return null;
+  }
+}
+
+function isExpectNode(node: Maybe<BaseObject>, expectType: objectType): boolean {
+  if (!isObject(node)) return false;
+  return node?.type() == expectType;
+}
+function isObject(node: Maybe<BaseObject>): boolean {
+  return node !== null;
 }
 function nativeBoolToBooleanObject(input: boolean): InternalBoolean {
   return input ? internal.TRUE : internal.FALSE;
