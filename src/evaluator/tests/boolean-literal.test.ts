f48eb91413e994d83729d0d0ad635895898256bc
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-08-15 12:57:59 -0300

    test(evaluator): extends my test

--- src/evaluator/tests/boolean-literal.test.ts
+++ src/evaluator/tests/boolean-literal.test.ts
@@ -12,6 +12,30 @@ describe("Evaluator", () => {
         input: "false",
         expected: false,
       },
+      {
+        input: "false != false",
+        expected: false,
+      },
+      {
+        input: "true != false",
+        expected: true,
+      },
+      {
+        input: "false == false",
+        expected: true,
+      },
+      {
+        input: `(1 < 2) == true`,
+        expected: true,
+      },
+      {
+        input: `(1 < 2) == false`,
+        expected: false,
+      },
+      {
+        input: `(1 > 2) == true`,
+        expected: false,
+      },
     ];
 
     tests.forEach((tt) => {
