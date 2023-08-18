ca889fc5e0049f0b9b5a93db0bf75689a816f2ca
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-08-18 05:44:31 +0200

    feat: error handler

---
+++ src/evaluator/errors/errors.ts
@@ -0,0 +1,26 @@
+import { BaseObject, EBaseObject, InternalError } from "evaluator/object";
+
+export function unknownOperatorError(operator: string): InternalError {
+  return new InternalError(`Unknown operator:  ${operator}`);
+}
+
+export function typeMismatchError(left: BaseObject, right: BaseObject, operator: string): InternalError {
+  return new InternalError(`Type mismatch: ${left.type()} ${operator} ${right.type()}`);
+}
+
+export function unknownIdentifierError(name: string): InternalError {
+  return new InternalError(`Unknown identifier: ${name}`);
+}
+
+export function parseTwoObjectToString(left: EBaseObject, right: EBaseObject, operator: string): string {
+  if (!left && right) {
+    return `${operator} ${right}`;
+  }
+  if (!right && left) {
+    return `${left} ${operator}`;
+  }
+  if (left && right) {
+    return `${left} ${operator} ${right}`;
+  }
+  throw new Error("left and right are null");
+}
