480db65cb98afe59290ab6678b38ab6a54d80448
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-08-19 19:01:07 +0200

    refactor: improve permission action

--- src/evaluator/permissions/index.ts
+++ src/evaluator/permissions/index.ts
@@ -1,28 +1,26 @@
 import { EBaseObject } from "evaluator/object";
-export type Permission = "==" | "!=" | "<" | ">" | "+" | "-" | "*" | "/" | "!";
+export type Operator = "==" | "!=" | "<" | ">" | "+" | "-" | "*" | "/" | "!";
 
-const PermissionOperator = new Map<Permission, Set<EBaseObject>>();
+const PermissionTable = new Map<Operator, Set<EBaseObject>>();
 
-export const SetPermission = (operator: Permission, permission: EBaseObject) => {
-  if (!PermissionOperator.has(operator)) {
-    PermissionOperator.set(operator, new Set<EBaseObject>());
+export const setPermission = (operator: Operator, permissions: EBaseObject[]) => {
+  if (!PermissionTable.has(operator)) {
+    PermissionTable.set(operator, new Set<EBaseObject>());
   }
-  PermissionOperator.get(operator)?.add(permission);
+  permissions.forEach((permission) => {
+    PermissionTable.get(operator)?.add(permission);
+  });
 };
-SetPermission("<", EBaseObject.INTEGER);
-SetPermission("<", EBaseObject.BOOLEAN);
-SetPermission(">", EBaseObject.INTEGER);
-SetPermission(">", EBaseObject.BOOLEAN);
-SetPermission("==", EBaseObject.INTEGER);
-SetPermission("==", EBaseObject.BOOLEAN);
-SetPermission("!=", EBaseObject.INTEGER);
-SetPermission("!=", EBaseObject.BOOLEAN);
-SetPermission("+", EBaseObject.INTEGER);
-SetPermission("-", EBaseObject.INTEGER);
-SetPermission("*", EBaseObject.INTEGER);
-SetPermission("/", EBaseObject.INTEGER);
-SetPermission("!", EBaseObject.BOOLEAN);
+setPermission("<", [EBaseObject.INTEGER, EBaseObject.BOOLEAN]);
+setPermission(">", [EBaseObject.INTEGER, EBaseObject.BOOLEAN]);
+setPermission("==", [EBaseObject.INTEGER, EBaseObject.BOOLEAN]);
+setPermission("!=", [EBaseObject.INTEGER, EBaseObject.BOOLEAN]);
+setPermission("+", [EBaseObject.INTEGER]);
+setPermission("-", [EBaseObject.INTEGER]);
+setPermission("*", [EBaseObject.INTEGER]);
+setPermission("/", [EBaseObject.INTEGER]);
+setPermission("!", [EBaseObject.BOOLEAN]);
 
 export const verifyPermission = (operator: string, objectType: EBaseObject): boolean => {
-  return Boolean(PermissionOperator.get(operator as Permission)?.has(objectType));
+  return Boolean(PermissionTable.get(operator as Operator)?.has(objectType));
 };
