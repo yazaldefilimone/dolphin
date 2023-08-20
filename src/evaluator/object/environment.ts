098ea3c0d13c489ab7b457ac1de950b8ed36a329
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-08-20 22:30:00 +0200

    feat(evaluator): enclose env

--- src/evaluator/object/environment.ts
+++ src/evaluator/object/environment.ts
@@ -4,13 +4,33 @@ import { BaseObject } from "./base";
 type EnvStoreType = Map<string, BaseObject>;
 
 export class Environment {
-  private envStore: EnvStoreType;
+  protected envStore: EnvStoreType;
   constructor() {
     this.envStore = new Map<string, BaseObject>();
   }
 
   public getStore(keyName: string): Maybe<BaseObject> {
     return this.envStore.get(keyName) ?? null;
+  }
+
+  public setStore(keyName: string, obj: BaseObject): void {
+    this.envStore.set(keyName, obj);
+  }
+}
+
+export class EnclosedEnvironment extends Environment {
+  private outer: Environment;
+  constructor(outer: Environment) {
+    super();
+    this.outer = outer;
+  }
+
+  public getStore(keyName: string): Maybe<BaseObject> {
+    const encloseEnv = this.envStore.get(keyName);
+    if (encloseEnv === undefined) {
+      return this.outer.getStore(keyName);
+    }
+    return encloseEnv;
   }
 
   public setStore(keyName: string, obj: BaseObject): void {
