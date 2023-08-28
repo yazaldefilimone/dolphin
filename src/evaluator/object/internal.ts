512c544efaf6d910426ab002453139aabde6fae6
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-08-28 18:31:19 -0300

    feat: register keyboard table in internal methods

--- src/evaluator/object/internal.ts
+++ src/evaluator/object/internal.ts
@@ -1,7 +1,9 @@
 import { InternalBoolean, InternalNull } from "evaluator/object";
+import { keyboardTable } from "./builtin";
 
 export const internal = {
   TRUE: new InternalBoolean(true),
   FALSE: new InternalBoolean(false),
   NULL: new InternalNull(),
+  keyboardTable: keyboardTable,
 };
